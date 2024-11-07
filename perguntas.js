fc8751c578bbf9aedd82c7050d6fbd0e24fd2396
Renatinnn1 <00001107530933sp@al.educacao.sp.gov.br>
2024-11-07 09:54:41 -0300

    final

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Biologia',
+    'QUem faz parte do reino monera?',
+    'Bactérias e cianobactérias'
 )
 
 criaCartao(
@@ -11,9 +11,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'Buenos Aires'
 )
 
 criaCartao(
